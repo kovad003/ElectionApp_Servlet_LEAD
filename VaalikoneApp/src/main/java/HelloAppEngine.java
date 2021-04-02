1e6908fec0f49c10326e1287ea496201e12cd4d2
lesheidrich <77836486+lesheidrich@users.noreply.github.com>
2021-04-02 22:22:10 +0300

    les

--- VaalikoneApp/src/main/java/HelloAppEngine.java
+++ VaalikoneApp/src/main/java/HelloAppEngine.java
@@ -18,7 +18,7 @@ public class HelloAppEngine extends HttpServlet {
     response.setContentType("text/plain");
     response.setCharacterEncoding("UTF-8");
 
-    response.getWriter().print("Puppies!\r\n");
+    response.getWriter().print("Les!\r\n");
 
   }
 }
