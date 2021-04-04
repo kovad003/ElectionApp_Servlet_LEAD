a9a313afe822f1e33ef04188c1e9d289572b9fc2
lesheidrich <77836486+lesheidrich@users.noreply.github.com>
2021-04-05 00:34:04 +0300

    crud update

--- VaalikoneApp/src/main/java/CRUD.java
+++ VaalikoneApp/src/main/java/CRUD.java
@@ -1,18 +1,15 @@
-import java.beans.Statement;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
-public class CRUD {
+class CRUD {
 	
-	/* v 1.0
-	 * author: Les Heidrich
-	 * email: lesheid@protonmail.com
+	/* CRUD class is designed to be run through instances for tables.
 	 * 
-	 * -= STATIC methods =-
-	 *  all methods are static and wrapped in try-catch so they can be
+	 * -= METHODS =-
+	 *  all methods are wrapped in try-catch so they can be
 	 *  run individually..
 	 * 	  - COMMAND: takes sql command -> e.g.: sql="create table test;"
 	 *    - insert into table
@@ -23,27 +20,31 @@ public class CRUD {
 	 * 
 	 * -= CONSTRUCTORS =-
 	 * 	  1) default:
-	 * 			-> initialized with default user settings
-	 * 			-> default vaalikone connection
+	 * 			-> takes no params
+	 * 			-> default user/pwd
+	 * 			-> default db: vaalikone
+	 * 			-> default table: kysymykset
 	 * 	  2) param.ized:
-	 * 			-> all 3 attrib take params in below spec order		
+	 * 			-> all 3 attrib take params in order:
+	 * 			     dbURL, username, password, table	
 	 */
 	
-	private static String dbURL;
-	private static String username;
-	private static String password;
+	protected String dbURL;
+	protected String username;
+	protected String password;
+	protected String table_name;
+			
 		
-	
 	public CRUD() {		
-		CRUD.dbURL = "jdbc:mysql://localhost:3306/vaalikone";
-		CRUD.username = "student";
-		CRUD.password = "Student1234?";
+		this.dbURL = "jdbc:mysql://localhost:3306/electionmachine";
+		this.username = "pena";
+		this.password = "kukkuu";
 	}
 	
 	public CRUD(String url, String user, String pwd) {		
-		CRUD.dbURL = url;
-		CRUD.username = user;
-		CRUD.password = pwd;
+		this.dbURL = url;
+		this.username = user;
+		this.password = pwd;
 	}
 	
 	
@@ -51,28 +52,28 @@ public class CRUD {
 	 ***************   GETTERS / SETTERS   *************
 	 ***************************************************/
 	
-	public static String getDbURL() {
+	public String getDbURL() {
 		return dbURL;
 	}
 
-	public static void setDbURL(String dbURL) {
-		CRUD.dbURL = dbURL;
+	public void setDbURL(String dbURL) {
+		this.dbURL = dbURL;
 	}
 
-	public static String getUsername() {
+	public String getUsername() {
 		return username;
 	}
 
-	public static void setUsername(String username) {
-		CRUD.username = username;
+	public void setUsername(String username) {
+		this.username = username;
 	}
 
-	public static String getPassword() {
+	public String getPassword() {
 		return password;
 	}
 
-	public static void setPassword(String password) {
-		CRUD.password = password;
+	public void setPassword(String password) {
+		this.password = password;
 	}
 
 	
@@ -80,170 +81,275 @@ public class CRUD {
 	 ********************   METHODS   ******************
 	 ***************************************************/
 	
-//	public static void command(String sql) {
-//		/*  Takes param of SQL string
-//		 *  Applies desired command to DB
-//		 *  Throws connection error
+	/*
+	*2) answers table: user answers -> String answers into this
+	*3) candidate table: answers -> returns answers Strings
+	*4) answers table: user answers -> returns answers String
+	*
+	*/
+	
+	public String[] getQ() {
+		/*
+		 * Returns -> array of question strings
+		 */
+		String arrQ[] = null;
+		try (Connection conn = DriverManager.getConnection(dbURL, username, password)) {
+			if (conn != null) {
+				System.out.println("select--connected");
+				
+				String script = "SELECT * FROM electionmachine.questions;";
+				java.sql.Statement statement = conn.createStatement();
+				ResultSet result = statement.executeQuery(script);
+				
+				int count = 0; 
+				arrQ = new String[19];
+				while (result.next()) {
+				    String id = result.getString("QUESTION_ID");
+				    String q = result.getString("QUESTION");
+				    arrQ[count] = q;
+//				    System.out.println(arrQ[count]);
+				}
+			}
+		}
+		catch (SQLException e) {
+			e.printStackTrace();
+			System.out.println("select--connection error!\n" + dbURL + "\nTable:" + 
+					"\nUser/Pwd:" + username + "/" + password);
+		}
+		return arrQ;
+	}
+	
+	
+	public String[] get_candA() {
+		/*
+		 * Returns -> array of question strings
+		 * 
+		 */
+		String arrCA[][] = null;
+		try (Connection conn = DriverManager.getConnection(dbURL, username, password)) {
+			if (conn != null) {
+				System.out.println("select--connected");
+				
+				String script = "SELECT * FROM electionmachine.answers;";
+				java.sql.Statement statement = conn.createStatement();
+				ResultSet result = statement.executeQuery(script);
+				
+				
+				/*
+				 * hashmap https://stackoverflow.com/questions/1540673/java-equivalent-to-python-dictionaries
+				 * hashmap -> [1, [1-19]]
+				 * next step: put hash map in for loop for length of politicians
+				 */
+				int count = 0; 
+				arrCA = new String[19][19];
+				while (result.next()) {
+				    String cid = result.getString("CANDIDATE_ID");
+				    String qid = result.getString("QUESTION_ID");
+				    String answer = result.getString("ANSWER");
+				    arrCA[count] = [cid, qid, answer];
+//				    System.out.println(arrQ[count]);
+				    
+				}
+			}
+		}
+		catch (SQLException e) {
+			e.printStackTrace();
+			System.out.println("select--connection error!\n" + dbURL + "\nTable:" + 
+					"\nUser/Pwd:" + username + "/" + password);
+		}
+		return arrCA;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+//		public void command(String sql) {
+//			/*  
+//			 * Input: SQL string to run
+//			 * Applies desired command to DB
+//			 * Throws: connection error, run error
+//			 */
+//			try {
+//				Connection conn = DriverManager.getConnection(dbURL, username, password);
+//				if (conn != null) {
+//					System.out.println("command--connected");
+//					
+//		            Statement statement = (Statement) conn.createStatement();
+//		            try {
+//		            	((java.sql.Statement) statement).executeUpdate(sql);
+//		            }catch (SQLException e) {
+//		            	e.printStackTrace();
+//		            	System.out.println("run error--command");
+//		            }     
+//				}
+//			} catch (SQLException e) {
+//				e.printStackTrace();
+//				System.out.println("insert--connection error!\n" + dbURL + 
+//						"\nUser/Pwd:" + username + "/" + password);
+//			}
+//		}
+	
+//	public void insert_data(String cid, String kid, String vst, String kmt) {
+//		/* 
+//		 * Input: all String -> 4 values to input into table
+//		 * Change: table/user/pwd/db with setter
+//		 * Throws: connection error, command error
+//		 * Confirms: connection, insert 
 //		 */
-//		try {
-//			Connection conn = DriverManager.getConnection(dbURL, username, password);
+//		try (Connection conn = DriverManager.getConnection(dbURL, username, password)) {
 //			if (conn != null) {
-//				System.out.println("command--connected");
+//				System.out.println("insert--connected");
 //				
-//	            Statement statement = (Statement) conn.createStatement();
-//	            try {
-//	            	((java.sql.Statement) statement).executeUpdate(sql);
-//	            }catch (SQLException e) {
-//	            	e.printStackTrace();
-//	            	System.out.println("run error--command");
-//	            }     
+//				String sql = "INSERT INTO vaalikone." + table_name + " (citizen_ID, kysymys_id, vastaus, kommentti) VALUES (?, ?, ?, ?)";
+//				try {
+//					PreparedStatement statement = conn.prepareStatement(sql);
+//					statement.setString(1, cid); 
+//					statement.setString(2, kid);
+//					statement.setString(3, vst);
+//					statement.setString(4, kmt);
+//
+//					int rowsInserted = statement.executeUpdate();
+//					if (rowsInserted > 0) {
+//						System.out.println("A new user was inserted into " + table_name.toUpperCase() + " successfully!");
+//					}
+//
+//				} catch (Exception e) {
+//					e.printStackTrace();
+//				}
+//				
 //			}
 //		} catch (SQLException e) {
 //			e.printStackTrace();
-//			System.out.println("Connection error--command!");
+//			System.out.println("insert--connection error!\n" + dbURL + "\nTable:" + table_name.toUpperCase() + 
+//					"\nUser/Pwd:" + username + "/" + password);
 //		}	
 //	}
 	
 	
-	public static void insert_data(String cid, String kid, String vst, String kmt) {
-		/*
-		 * 
-		 */
-		try {
-			Connection conn = DriverManager.getConnection(dbURL, username, password);
-			if (conn != null) {
-				System.out.println("insert--connected");
-				
-				String sql = "INSERT INTO vaalikone.citizenkane (citizen_ID, kysymys_id, vastaus, kommentti) VALUES (?, ?, ?, ?)";
-				try {
-					PreparedStatement statement = conn.prepareStatement(sql);
-					statement.setString(1, cid); 
-					statement.setString(2, kid);
-					statement.setString(3, vst);
-					statement.setString(4, kmt);
-
-					int rowsInserted = statement.executeUpdate();
-					if (rowsInserted > 0) {
-						System.out.println("A new user was inserted successfully!");
-					}
-
-				} catch (Exception e) {
-					// TODO: handle exception
-				}
-				
-			}
-		} catch (SQLException e) {
-			e.printStackTrace();
-			System.out.println("insert--connection error!");
-		}	
-	}
-	
-	
-//	public static void select_data() {
+//	public void select_data(String sql) {
 //		/*
+//		 * Input: param -> sql string from table name to additional options
+//		 * 		  existing sql string to append: "SELECT * FROM "..
+//		 * Throws: Connection error
 //		 * 
 //		 */
-//		try {
-//			Connection conn = DriverManager.getConnection(dbURL, username, password);
+//		try (Connection conn = DriverManager.getConnection(dbURL, username, password)) {
 //			if (conn != null) {
-//				System.out.println("Connected");
+//				System.out.println("select--connected");
 //				
-//				String sql = "SELECT * FROM Users";
-//				int count = 0;
+//				String script = "SELECT * FROM " + sql;
+//				java.sql.Statement statement = conn.createStatement();
+//				ResultSet result = statement.executeQuery(script);
 //
-//				try {
-//					Statement statement = conn.createStatement();
-//					ResultSet result = statement.executeQuery(sql);
-//
-//					while (result.next()) {
-//						String name = result.getString(2); // using getString -version relying on column index
-//						String pass = result.getString(3);
-//						String fullname = result.getString("fullname"); // using getString -version relying on column name
-//						String email = result.getString("email");
-//
-//						String output = "User #%d: %s - %s - %s - %s";
-//						System.out.println(String.format(output, ++count, name, pass, fullname, email));
-//					}
-//
-//				} catch (Exception e) {
-//					// TODO: handle exception
+//				int count = 0; 
+//				while (result.next()) {
+//				    String name = result.getString(2);
+//				    String pass = result.getString(3);
+//				    String fullname = result.getString("fullname");
+//				    String email = result.getString("email");
+//				 
+//				    String output = "User #%d: %s - %s - %s - %s";
+//				    System.out.println(String.format(output, ++count, name, pass, fullname, email));
 //				}
 //				
 //			}
 //		} catch (SQLException e) {
 //			e.printStackTrace();
-//			System.out.println("Connection error!");
+//			System.out.println("select--connection error!\n" + dbURL + "\nTable:" + 
+//					"\nUser/Pwd:" + username + "/" + password);
 //		}
 //	}
 	
 	
-//	public static void update_data() {
-//		/*
-//		 * 
-//		 */
-//		try {
-//			Connection conn = DriverManager.getConnection(dbURL, username, password);
-//			if (conn != null) {
-//				System.out.println("Connected");
-//				
-//				String sql = "UPDATE Users SET password=?, fullname=?, email=? WHERE username=?";
-//				try {
-//					PreparedStatement statement = conn.prepareStatement(sql);
+//		public static void update_data() {
+//			/*
+//			 * 
+//			 */
+//			try {
+//				Connection conn = DriverManager.getConnection(dbURL, username, password);
+//				if (conn != null) {
+//					System.out.println("Connected");
+//					
+//					String sql = "UPDATE Users SET password=?, fullname=?, email=? WHERE username=?";
+//					try {
+//						PreparedStatement statement = conn.prepareStatement(sql);
 //
-//					statement.setString(1, password);
-//					statement.setString(2, fullname);
-//					statement.setString(3, email);
-//					statement.setString(4, username);
+//						statement.setString(1, password);
+//						statement.setString(2, fullname);
+//						statement.setString(3, email);
+//						statement.setString(4, username);
 //
-//					int rowsUpdated = statement.executeUpdate();
-//					if (rowsUpdated > 0) {
-//						System.out.println("An existing user was updated successfully!");
+//						int rowsUpdated = statement.executeUpdate();
+//						if (rowsUpdated > 0) {
+//							System.out.println("An existing user was updated successfully!");
+//						}
+//
+//					} catch (Exception e) {
+//						// TODO: handle exception
 //					}
-//
-//				} catch (Exception e) {
-//					// TODO: handle exception
+//					
 //				}
-//				
+//			} catch (SQLException e) {
+//				e.printStackTrace();
+//				System.out.println("Connection error!");
 //			}
-//		} catch (SQLException e) {
-//			e.printStackTrace();
-//			System.out.println("Connection error!");
 //		}
-//	}
  
 	
-//	public static void delete_data() {
-//		/*
-//		 * 
-//		 */
-//		try {
-//			Connection conn = DriverManager.getConnection(dbURL, username, password);
-//			if (conn != null) {
-//				System.out.println("Connected");
-//				
-//				String sql = "DELETE FROM Users WHERE username=?";
-//
-//				try {
-//					PreparedStatement statement = conn.prepareStatement(sql);
+//		public static void delete_data() {
+//			/*
+//			 * 
+//			 */
+//			try {
+//				Connection conn = DriverManager.getConnection(dbURL, username, password);
+//				if (conn != null) {
+//					System.out.println("Connected");
 //					
-//					statement.setString(1, username);
-//					 
-//					int rowsDeleted = statement.executeUpdate();
-//					if (rowsDeleted > 0) {
-//					    System.out.println("A user was deleted successfully!");
+//					String sql = "DELETE FROM Users WHERE username=?";
+//
+//					try {
+//						PreparedStatement statement = conn.prepareStatement(sql);
+//						
+//						statement.setString(1, username);
+//						 
+//						int rowsDeleted = statement.executeUpdate();
+//						if (rowsDeleted > 0) {
+//						    System.out.println("A user was deleted successfully!");
+//						}
+//						
+//						
+//					} catch (Exception e) {
+//						// TODO: handle exception
 //					}
 //					
-//					
-//				} catch (Exception e) {
-//					// TODO: handle exception
 //				}
-//				
+//			} catch (SQLException e) {
+//				e.printStackTrace();
+//				System.out.println("Connection error!");
 //			}
-//		} catch (SQLException e) {
-//			e.printStackTrace();
-//			System.out.println("Connection error!");
 //		}
-//	}
 	
 }//end CRUD class
+
+
 
--- VaalikoneApp/src/main/java/Test.java
+++ VaalikoneApp/src/main/java/Test.java
@@ -1,127 +1,249 @@
-package zDB_TEST;
-
+import java.beans.Statement;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-import java.sql.Statement;
+import java.sql.SQLException;
 
+public class CRUD {
+	
+	/* v 1.0
+	 * author: Les Heidrich
+	 * email: lesheid@protonmail.com
+	 * 
+	 * -= STATIC methods =-
+	 *  all methods are static and wrapped in try-catch so they can be
+	 *  run individually..
+	 * 	  - COMMAND: takes sql command -> e.g.: sql="create table test;"
+	 *    - insert into table
+	 *    - select from table
+	 *    - alter table
+	 *    - update table
+	 *    - delete table
+	 * 
+	 * -= CONSTRUCTORS =-
+	 * 	  1) default:
+	 * 			-> initialized with default user settings
+	 * 			-> default vaalikone connection
+	 * 	  2) param.ized:
+	 * 			-> all 3 attrib take params in below spec order		
+	 */
+	
+	private static String dbURL;
+	private static String username;
+	private static String password;
+		
+	
+	public CRUD() {		
+		CRUD.dbURL = "jdbc:mysql://localhost:3306/vaalikone";
+		CRUD.username = "student";
+		CRUD.password = "Student1234?";
+	}
+	
+	public CRUD(String url, String user, String pwd) {		
+		CRUD.dbURL = url;
+		CRUD.username = user;
+		CRUD.password = pwd;
+	}
+	
+	
+	/***************************************************
+	 ***************   GETTERS / SETTERS   *************
+	 ***************************************************/
+	
+	public static String getDbURL() {
+		return dbURL;
+	}
 
-public class Main {
+	public static void setDbURL(String dbURL) {
+		CRUD.dbURL = dbURL;
+	}
 
-	public static void main(String[] args) {
-		System.out.println("puppyfarts");
-		
-		String url = "jdbc:mysql://localhost:3306/vaalikone";
-		String user = "student";
-		String pwd = "Student1234?";
-		
-//		CRUDtest.insert_data(url, user, pwd, "1", "1", "1", "geez");
-		
-		
+	public static String getUsername() {
+		return username;
+	}
+
+	public static void setUsername(String username) {
+		CRUD.username = username;
+	}
+
+	public static String getPassword() {
+		return password;
+	}
+
+	public static void setPassword(String password) {
+		CRUD.password = password;
+	}
+
+	
+	/***************************************************
+	 ********************   METHODS   ******************
+	 ***************************************************/
+	
+//	public static void command(String sql) {
+//		/*  Takes param of SQL string
+//		 *  Applies desired command to DB
+//		 *  Throws connection error
+//		 */
 //		try {
-//			// 1. Get a connection to database
-//			Connection myConn = DriverManager.getConnection(url, user, pwd);
-//			
-//			//2. create statement
-//			Statement myStmt = myConn.createStatement();
-//			
-//			//3. execute sql query
-//			ResultSet myRs = myStmt.executeQuery("SELECT * FROM vaalikone.kysymykset;");
-//			
-//			// 4. Process the result set
-//			while (myRs.next()) {
-//				System.out.println(myRs.getString("KYSYMYS_ID") + ": " + myRs.getString("KYSYMYS"));			}
-//		}
-//		catch (Exception exc) {
-//			exc.printStackTrace();
-//		}
-
-
-			
-
-		
-	}//main
-}
-
-
-class Fudge {
+//			Connection conn = DriverManager.getConnection(dbURL, username, password);
+//			if (conn != null) {
+//				System.out.println("command--connected");
+//				
+//	            Statement statement = (Statement) conn.createStatement();
+//	            try {
+//	            	((java.sql.Statement) statement).executeUpdate(sql);
+//	            }catch (SQLException e) {
+//	            	e.printStackTrace();
+//	            	System.out.println("run error--command");
+//	            }     
+//			}
+//		} catch (SQLException e) {
+//			e.printStackTrace();
+//			System.out.println("Connection error--command!");
+//		}	
+//	}
 	
-	String url = "jdbc:mysql://localhost:3306/vaalikone";
-	String user = "student";
-	String pwd = "Student1234?";
-
+	
+	public static void insert_data(String cid, String kid, String vst, String kmt) {
+		/*
+		 * 
+		 */
 		try {
-		// 1. Get a connection to database
-		Connection myConn = DriverManager.getConnection(url, user, pwd);
-		
-		//2. create statement
-		Statement myStmt = myConn.createStatement();
-		
-		//3. execute sql query
-		ResultSet myRs = myStmt.executeQuery("SELECT * FROM vaalikone.kysymykset;");
-		
-		// 4. Process the result set
-		while (myRs.next()) {
-			System.out.println(myRs.getString("KYSYMYS_ID") + ": " + myRs.getString("KYSYMYS"));			
+			Connection conn = DriverManager.getConnection(dbURL, username, password);
+			if (conn != null) {
+				System.out.println("insert--connected");
+				
+				String sql = "INSERT INTO vaalikone.citizenkane (citizen_ID, kysymys_id, vastaus, kommentti) VALUES (?, ?, ?, ?)";
+				try {
+					PreparedStatement statement = conn.prepareStatement(sql);
+					statement.setString(1, cid); 
+					statement.setString(2, kid);
+					statement.setString(3, vst);
+					statement.setString(4, kmt);
+
+					int rowsInserted = statement.executeUpdate();
+					if (rowsInserted > 0) {
+						System.out.println("A new user was inserted successfully!");
+					}
+
+				} catch (Exception e) {
+					// TODO: handle exception
+				}
+				
 			}
-	}
-	catch (Exception exc) {
-		exc.printStackTrace();
+		} catch (SQLException e) {
+			e.printStackTrace();
+			System.out.println("insert--connection error!");
+		}	
 	}
 	
-	}
-//	
-//	
-//	
-//	try {
-//		// 1. Get a connection to database
-//		Connection myConn = DriverManager.getConnection(url, user, pwd);
-//		
-//		//2. create statement
-//		Statement myStmt = myConn.createStatement();
-//		
-//		//3. execute sql query
-//		ResultSet myRs = myStmt.executeQuery("SELECT * FROM vaalikone.kysymykset;");
-//		
-//		// 4. Process the result set
-//		while (myRs.next()) {
-//			System.out.println(myRs.getString("KYSYMYS_ID") + ": " + myRs.getString("KYSYMYS"));			}
-//	}
-//	catch (Exception exc) {
-//		exc.printStackTrace();
-//	}
-//}
-
-
-//class Fudge {
-//	public String url = "jdbc:mysql://localhost:3306/vaalikone";
-//	public String user = "student";
-//	public String paswd = "Student1234?";
-//	
-//	try {
-//		Connection conn = DriverManager.getConnection(url, user, paswd);
-//		if (conn != null) {
-//			System.out.println("insert--connected");
-//			
-//			String sql = "INSERT INTO vaalikone.citizenkane (citizen_ID, kysymys_id, vastaus, kommentti) VALUES (?, ?, ?, ?)";
-//			try {
-//				PreparedStatement statement = conn.prepareStatement(sql);
-//				statement.setString(1, "1"); 
-//				statement.setString(2, "1");
-//				statement.setString(3, "1");
-//				statement.setString(4, "looody");
+	
+//	public static void select_data() {
+//		/*
+//		 * 
+//		 */
+//		try {
+//			Connection conn = DriverManager.getConnection(dbURL, username, password);
+//			if (conn != null) {
+//				System.out.println("Connected");
+//				
+//				String sql = "SELECT * FROM Users";
+//				int count = 0;
 //
-//				int rowsInserted = statement.executeUpdate();
-//				if (rowsInserted > 0) {
-//					System.out.println("A new user was inserted successfully!");
+//				try {
+//					Statement statement = conn.createStatement();
+//					ResultSet result = statement.executeQuery(sql);
+//
+//					while (result.next()) {
+//						String name = result.getString(2); // using getString -version relying on column index
+//						String pass = result.getString(3);
+//						String fullname = result.getString("fullname"); // using getString -version relying on column name
+//						String email = result.getString("email");
+//
+//						String output = "User #%d: %s - %s - %s - %s";
+//						System.out.println(String.format(output, ++count, name, pass, fullname, email));
 //					}
-//				}catch (Exception e) {
-//					e.printStackTrace();
+//
+//				} catch (Exception e) {
+//					// TODO: handle exception
 //				}
-//			
+//				
+//			}
+//		} catch (SQLException e) {
+//			e.printStackTrace();
+//			System.out.println("Connection error!");
 //		}
-//	}catch (Exception exc) {
-//			exc.printStackTrace();}
-//	
-//}
+//	}
+	
+	
+//	public static void update_data() {
+//		/*
+//		 * 
+//		 */
+//		try {
+//			Connection conn = DriverManager.getConnection(dbURL, username, password);
+//			if (conn != null) {
+//				System.out.println("Connected");
+//				
+//				String sql = "UPDATE Users SET password=?, fullname=?, email=? WHERE username=?";
+//				try {
+//					PreparedStatement statement = conn.prepareStatement(sql);
+//
+//					statement.setString(1, password);
+//					statement.setString(2, fullname);
+//					statement.setString(3, email);
+//					statement.setString(4, username);
+//
+//					int rowsUpdated = statement.executeUpdate();
+//					if (rowsUpdated > 0) {
+//						System.out.println("An existing user was updated successfully!");
+//					}
+//
+//				} catch (Exception e) {
+//					// TODO: handle exception
+//				}
+//				
+//			}
+//		} catch (SQLException e) {
+//			e.printStackTrace();
+//			System.out.println("Connection error!");
+//		}
+//	}
+ 
+	
+//	public static void delete_data() {
+//		/*
+//		 * 
+//		 */
+//		try {
+//			Connection conn = DriverManager.getConnection(dbURL, username, password);
+//			if (conn != null) {
+//				System.out.println("Connected");
+//				
+//				String sql = "DELETE FROM Users WHERE username=?";
+//
+//				try {
+//					PreparedStatement statement = conn.prepareStatement(sql);
+//					
+//					statement.setString(1, username);
+//					 
+//					int rowsDeleted = statement.executeUpdate();
+//					if (rowsDeleted > 0) {
+//					    System.out.println("A user was deleted successfully!");
+//					}
+//					
+//					
+//				} catch (Exception e) {
+//					// TODO: handle exception
+//				}
+//				
+//			}
+//		} catch (SQLException e) {
+//			e.printStackTrace();
+//			System.out.println("Connection error!");
+//		}
+//	}
+	
+}//end CRUD class
+
