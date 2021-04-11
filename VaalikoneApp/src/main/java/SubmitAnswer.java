ef2e996e906c638cf01b727258b40febda533b3d
Daniel Kovacs <kovacsdansan@gmail.com>
2021-04-11 14:20:32 +0300

    assessing algorithm is working, can be displayed on a jsp
    - have to add some comments

--- VaalikoneApp/src/main/java/SubmitAnswer.java
+++ VaalikoneApp/src/main/java/SubmitAnswer.java
@@ -11,7 +11,9 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import dao.Dao_candidate;
 import dao.Dao_qanswer;
+import data.Candidate;
 import data.QAnswer;
 
 /**
@@ -20,11 +22,14 @@ import data.QAnswer;
 @WebServlet("/SubmitAnswer")
 public class SubmitAnswer extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-	private Dao_qanswer dao=null;
+	private Dao_qanswer dao_qanswer=null;
+	private Dao_candidate dao_candidate=null;
 	
 	@Override
 	public void init() {
-		dao=new Dao_qanswer("jdbc:mysql://localhost:3306/electionmachine", "pena", "kukkuu");
+		dao_qanswer=new Dao_qanswer("jdbc:mysql://localhost:3306/electionmachine", "pena", "kukkuu");
+		System.out.println("");
+		dao_candidate=new Dao_candidate("jdbc:mysql://localhost:3306/electionmachine", "pena", "kukkuu");
 		System.out.println("");
 	}
        
@@ -73,45 +78,87 @@ public class SubmitAnswer extends HttpServlet {
 //		*********** GET CANDIDATE SELECTIONS/ANSWERS ************************************************************************************************
 //		*********************************************************************************************************************************************
 		ArrayList<QAnswer> answerList=null;
-		if(dao.getConnection())
+		if(dao_qanswer.getConnection())
 		{
 			System.out.println("Successfully connected to the database");
-			answerList=dao.readAllAnswer();
+			answerList=dao_qanswer.readAllAnswer();
 			System.out.println("Answer_List: " + answerList);
-			
-			for (int i = 0; i < answerList.size(); i++) {		
-				QAnswer a = answerList.get(i);
-				a.getAnswer();
-				
-//				<<< Debugging Messages >>>
+	
+//			<<< Debugging Messages >>>
+//			for (int i = 0; i < answerList.size(); i++) {		
+//				QAnswer a = answerList.get(i);
+//				a.getAnswer();
 //				System.out.println("Candidate ID: " + a.getCId());
 //				System.out.println("Question ID: " + a.getQId());
 //				System.out.println("Answer : " + a.getAnswer());
-			}	
+//			}	
+		}
+		else
+		{
+			System.out.println("No connection to database");
+		}
+		
+//		*********************************************************************************************************************************************
+//		************ GET ALL CANDIDATE PROFILE ******************************************************************************************************
+//		*********************************************************************************************************************************************	
+		ArrayList<Candidate> candidateProfileStacked=null;
+		if(dao_candidate.getConnection())
+		{
+			System.out.println("Successfully connected to the database");
+			candidateProfileStacked=dao_candidate.readAllCandidate();
+			System.out.println("Can_List: " + candidateProfileStacked);
+			
+//			<<< Debugging Messages >>> 		
+//			for (int i = 0; i < candidateList.size(); i++) {
+//				Candidate c = candidateList.get(i);//		
+//				System.out.println("Candidate name: " + c.getFName() + " " + c.getSName());
+//				System.out.println("location: " + c.getLocation() );
+//			}	
 		}
 		else
 		{
 			System.out.println("No connection to database");
 		}
 
-		
 //		*********************************************************************************************************************************************
-//		************ COMPARE CLIENT WITH CANDIDATES *************************************************************************************************
+//		************ PREPARE QUESTIONNAIRE RESULT****************************************************************************************************
 //		*********************************************************************************************************************************************	
 		ArrayList<QAnswer> answerListScored = scoreStackedData(answerList, selectionList); //Will produce a "stacked" ArrayList containing all the QA objects with individual score values.
 		ArrayList<QAnswer> scoreBoard = findBestCnds(answerListScored);
-		ArrayList<QAnswer> result1 = sliceFromStacked(0, scoreBoard, answerListScored);
-		ArrayList<QAnswer> result2 = sliceFromStacked(1, scoreBoard, answerListScored);
-		ArrayList<QAnswer> result3 = sliceFromStacked(2, scoreBoard, answerListScored);
-
+		
+		int cndIdPole = getCndIdFromScoreBoard(1, scoreBoard);
+		int cndIdSecond = getCndIdFromScoreBoard(2, scoreBoard);
+		int cndIdThird = getCndIdFromScoreBoard(3, scoreBoard);
+		
+		ArrayList<QAnswer> result_1st = sliceFromStacked(cndIdPole, answerListScored);
+		ArrayList<QAnswer> result_2nd = sliceFromStacked(cndIdSecond, answerListScored);
+		ArrayList<QAnswer> result_3rd = sliceFromStacked(cndIdThird, answerListScored);
+		
+//		<<< Debugging Messages >>> 	
+//		System.out.println("Top1 CID: " + cndIdPole);
+//		System.out.println("Top1 CID: " + cndIdSecond);
+//		System.out.println("Top1 CID: " + cndIdThird);
 		
 //		*********************************************************************************************************************************************
-//		************ SENDING DATA TO THE JSP PAGE *************************************************************************************************
-//		*********************************************************************************************************************************************
+//		************ PREPARE TOP CANDIDATE PROFILES *************************************************************************************************
+//		*********************************************************************************************************************************************	
 
-		request.setAttribute("topCnd_1", result1);
-		request.setAttribute("topCnd_2", result2);
-		request.setAttribute("topCnd_3", result3);
+		Candidate profile_1st = returnCndProfile(cndIdPole, candidateProfileStacked);
+		Candidate profile_2nd = returnCndProfile(cndIdSecond, candidateProfileStacked);
+		Candidate profile_3rd = returnCndProfile(cndIdThird, candidateProfileStacked);
+		
+		
+//		*********************************************************************************************************************************************
+//		************ SENDING DATA TO THE JSP PAGE ***************************************************************************************************
+//		*********************************************************************************************************************************************
+		
+		request.setAttribute("result_1st", result_1st);
+		request.setAttribute("result_2nd", result_2nd);
+		request.setAttribute("result_3rd", result_3rd);
+		
+		request.setAttribute("profile_1st", profile_1st);
+		request.setAttribute("profile_2nd", profile_2nd);
+		request.setAttribute("profile_3rd", profile_3rd);
 		
 		RequestDispatcher rd=request.getRequestDispatcher("/myCandidate.jsp");
 		rd.forward(request, response);
@@ -178,9 +225,9 @@ public class SubmitAnswer extends HttpServlet {
 	
 	public ArrayList<QAnswer> findBestCnds(ArrayList<QAnswer> scoredStackedData)
 	{	
-		ArrayList<QAnswer> cndsToSlice = new ArrayList<QAnswer>();// Will be returned at the end.
-		ArrayList<QAnswer> stackedArrLi = scoredStackedData; 
-		ListIterator<QAnswer> iterator = stackedArrLi.listIterator(); // Will iterate through the ArrayList.
+		ArrayList<QAnswer> cndScoreBoard = new ArrayList<QAnswer>();// Will be returned at the end.
+		ArrayList<QAnswer> scoredStack = scoredStackedData; 
+		ListIterator<QAnswer> iterator = scoredStack.listIterator(); // Will iterate through the ArrayList.
 		
 //		############ DROP FALSE DATA #############			
 		while(iterator.hasNext()) // Will remove "junk" (false) total score data (-1).
@@ -188,7 +235,7 @@ public class SubmitAnswer extends HttpServlet {
 			QAnswer object = new QAnswer();
 			object = iterator.next();
 			if(object.getTotalScore() >= 0) {
-				cndsToSlice.add(object);
+				cndScoreBoard.add(object);
 			}
 			else {
 //				<<< Debugging Messages >>> 	
@@ -197,24 +244,36 @@ public class SubmitAnswer extends HttpServlet {
 		}
 
 //		############ SORTING BASED ON TOTAL SCORE #############	
-        Collections.sort(cndsToSlice, new Comparator<QAnswer>() { // Will organise elements into ascending order.
+        Collections.sort(cndScoreBoard, new Comparator<QAnswer>() { // Will organise elements into ascending order.
             @Override public int compare(QAnswer o1, QAnswer o2) {
                 return o1.getTotalScore() - o2.getTotalScore(); }}); 
         
 //		<<< Debugging Messages >>> 		
-		for (int i = 0; i < cndsToSlice.size(); i++) {
-			System.out.println("scoring results: " + "CID: " + cndsToSlice.get(i).getCId() + 
-					", Total score: " + cndsToSlice.get(i).getTotalScore());}
+		for (int i = 0; i < cndScoreBoard.size(); i++) {
+			System.out.println("scoring results: " + "CID: " + cndScoreBoard.get(i).getCId() + 
+					", Total score: " + cndScoreBoard.get(i).getTotalScore());}
 	
-		return cndsToSlice;
+		return cndScoreBoard;
 	}
 	
 	
-	public ArrayList<QAnswer> sliceFromStacked(int rankAchieved, ArrayList<QAnswer> scoreBoard, ArrayList<QAnswer> scoredStackedData)
+	public int getCndIdFromScoreBoard(int rankAchieved, ArrayList<QAnswer> scoreBoard)
 	{
-		int ranking = rankAchieved;
-		ArrayList<QAnswer> resultsArrLi = new ArrayList<QAnswer>();
-		ArrayList<QAnswer> scores = scoreBoard;
+		ArrayList<QAnswer> scBoard = scoreBoard;
+		
+		int ranking = rankAchieved -1;
+		int candidateId;
+		
+		candidateId = scBoard.get(ranking).getCId();	
+		
+		return candidateId;	
+	}
+	
+	
+	public ArrayList<QAnswer> sliceFromStacked(int candidateId, ArrayList<QAnswer> scoredStackedData)
+	{
+		int cndId = candidateId;
+		ArrayList<QAnswer> result = new ArrayList<QAnswer>();
 		ArrayList<QAnswer> scoredStackedArrLi = scoredStackedData; 
 		ListIterator<QAnswer> iterator = scoredStackedArrLi.listIterator(); // Will reset iterator.
 		
@@ -222,173 +281,41 @@ public class SubmitAnswer extends HttpServlet {
 			QAnswer object = new QAnswer();
 			object = iterator.next();
 			
-			if(object.getCId() == scores.get(ranking).getCId()) {
-				resultsArrLi.add(object);
+			if(object.getCId() == cndId) {
+				result.add(object);
 			}
 		}
 		
 //		<<< Debugging Messages >>> 		
-		for (int i = 0; i < resultsArrLi.size(); i++) {
-			System.out.println("Cnd data sliced: " + "CID: " + resultsArrLi.get(i).getCId() + 
-					", QID: " + resultsArrLi.get(i).getQId() + ", Score: " + resultsArrLi.get(i).getScore() + 
-					", Total score: " + resultsArrLi.get(i).getTotalScore() + ", Q-TXT: " + resultsArrLi.get(i).getQTxt());}
+		for (int i = 0; i < result.size(); i++) {
+			System.out.println("Cnd data sliced: " + "CID: " + result.get(i).getCId() + 
+					", QID: " + result.get(i).getQId() + ", Score: " + result.get(i).getScore() + 
+					", Total score: " + result.get(i).getTotalScore() + ", Q-TXT: " + result.get(i).getQTxt());}
 		
-		return resultsArrLi;
+		return result;
 	}
 	
 	
-/*	
-	public int[] findTopCandidates(ArrayList<QAnswer> answerList, ArrayList<QAnswer> selectionList)
+	public Candidate returnCndProfile(int candidateId, ArrayList<Candidate> candidateProfileStacked)
 	{
-		ArrayList<QAnswer> RankedArrList = new ArrayList<QAnswer>();
-		ArrayList<QAnswer> candidateScoredArrLi = new ArrayList<QAnswer>();
+		int cndId = candidateId;
+//		ArrayList<Candidate> profile = new ArrayList<Candidate>();
+		ArrayList<Candidate> cndProfileStck = candidateProfileStacked; 
+		ListIterator<Candidate> iterator = cndProfileStck.listIterator();
 		
+		Candidate profile = new Candidate();
 		
-		Set<Integer> avlblCID = listAvlblCandidates(selectionList, answerList); // Will return a SET with the available Candidate IDs (CID)		
-        Iterator<Integer> iterator = avlblCID.iterator(); // Creating an iterator object.
-//		<<< Debugging Messages >>>
-//      System.out.println("The iterator values are: "); // Displaying the values after iterating through the iterator
-
-        while (iterator.hasNext()) {
-        	Integer myInteger = iterator.next();
-            int c_id = myInteger.intValue();
-            System.out.println(c_id);
-            
-            candidateScoredArrLi = scoringCandidate(c_id, answerList, selectionList);
-            QAnswer lastItem = candidateScoredArrLi.get(candidateScoredArrLi.size() - 1);
-            
-            RankedArrList.add(lastItem);
-
-            lastItem.getTotalScore();
-            lastItem.getAnswer();
-            
-            System.out.println("Score: " + lastItem.getTotalScore());
-        }
-
-        Collections.sort(RankedArrList, new Comparator<QAnswer>() { // Will organise elements into ascending order.
-            @Override public int compare(QAnswer o1, QAnswer o2) {
-                return o1.getTotalScore() - o2.getTotalScore(); }}); 
-        
-        int top_3[] = new int[RankedArrList.size()]; // Top 3 candidate ID (CID) will be returned in an array. 
-		for(int i=0; i < RankedArrList.size(); i++){			
-			top_3[i] = RankedArrList.get(i).getCId();
+		while(iterator.hasNext()) {
+			Candidate temporary = new Candidate();
+			temporary = iterator.next();
 			
-//			<<< Debugging Messages >>>   	  
-//			System.out.println("CID: " + RankedArrList.get(i).getCId() + "Total: " + RankedArrList.get(i).getTotalScore() );
-		  }
-		return top_3;  
-	}
-	
-	
-	
-//	Will score the chosen candidate (based on the c_id or CID) from the stacked ArrayList of candidate objects.
-//	An ArrayList will be returned containing the relevant data for the candidate, including the totalscore.
-//	############## IMPORTANT NOTE!!! Only use the last element of the AL to get the total score ###############
-	public ArrayList<QAnswer> scoringCandidate(int c_id, ArrayList<QAnswer> answerList, ArrayList<QAnswer> selectionList)
-	{
-		int id_num = c_id; // c_id have to be initialised as a new integer. If c_id is used as a condition in the control flow " == " will re-assign its value => Error!
-		ArrayList<QAnswer> candidateScoredArrLi = new ArrayList<QAnswer>(); // Will be returned at the end.
-		ListIterator<QAnswer> iterator = answerList.listIterator(); // Will iterate through the ArrayList.
-		ListIterator<QAnswer> iterator2 = selectionList.listIterator(); // Will iterate through the ArrayList.
-
-//		<<< Debugging Messages >>>
-//		System.out.println("answerList: " + answerList);
-//		System.out.println("iterator: " + iterator);
-		
-		int totalScore = 0; // Has to be initialised outside while loop!
-		while(iterator.hasNext())
-		{
-			int score = 0; // Will be reseted every turn.
-			QAnswer candidateSingle = iterator.next(); // Will select the next object -first, when we start the iteration- in the ArrayList. 
-//			Will create an empty object for the candidate, which will be amended with relevant data.
-			
-//			<<< Debugging Messages >>>
-//			System.out.println("c_id: " + id_num +  " CID: " + candidateSingle.getCId());
-			if (id_num == candidateSingle.getCId()) { // Will amend QAnswer objects with attributes.
-				candidateSingle.getCId();
-				candidateSingle.getQId();
-				int answerCnddt = candidateSingle.getAnswer();
-				
-				// Scoring system based on client's data
-				if (iterator2.hasNext()) {
-					QAnswer client = iterator2.next(); // Will select the next object -first, when we start the iteration- in the ArrayList. Will create an empty object for the client, which will be amended with relevant data.
-					client.getQId();
-					int answerClnt = client.getAnswer();
-					
-					score = Math.abs(answerClnt - answerCnddt); // We use the absolute value of the difference.
-					candidateSingle.setScore(score);
-					totalScore = totalScore + score;
-					candidateSingle.setTotalScore(totalScore); // Only use the last object of the ArrayList to check the total score, it grows as the iteration goes!
-					candidateSingle.setTotalScoreAsInteger(totalScore);
-				}
-				else
-				{
-					System.out.println("No user selection!");
-				}
-				
-				// ARRAYLIST:
-				candidateScoredArrLi.add(candidateSingle); // Will add the QAnswer object to the ArrayList
-
-//				<<< Debugging Messages >>>
-//				System.out.println("iterator2.hasNext(): " + iterator2.hasNext());
-//				System.out.println("CID: " + candidateSingle.getCId() + " - QID: " + 
-//				candidateSingle.getQId() + " - A: " + candidateSingle.getAnswer());
-//				System.out.println("Score: " + score);
+			if(temporary.getId() == cndId) {
+				profile = temporary;
 			}
-		}
-//		<<< Debugging Messages >>>
-		System.out.println("Total: " + totalScore);
-		return candidateScoredArrLi;
-	}
-
-//	Will extract the data of the chosen candidate (based on the c_id or CID) from the stacked ArrayList into a separate ArrayList.
-//	An ArrayList will be returned containing the relevant data for the candidate.
-	public ArrayList<QAnswer> extractCandidate(int c_id, ArrayList<QAnswer> answerList)
-	{
-		int id_num = c_id; // c_id have to be initialised as a new integer. If c_id is used as a condition in the control flow " == " will re-assign its value => Error!
-		ArrayList<QAnswer> candidateSingleArrList = new ArrayList<QAnswer>(); // Will be returned at the end.
-		ListIterator<QAnswer> iterator = answerList.listIterator(); // Will iterate through the ArrayList.
-
-//		<<< Debugging Messages >>>
-//		System.out.println("answerList: " + answerList);
-//		System.out.println("iterator: " + iterator);
+		}	
 		
-		while(iterator.hasNext())
-		{
-			QAnswer candidateSingle = iterator.next(); // Will select the next object -first, when we start the iteration- in the ArrayList. 
-//			Will create an empty object for the candidate, which will be amended with relevant data.
-			
-//			<<< Debugging Messages >>>
-//			System.out.println("c_id: " + id_num +  " CID: " + candidateSingle.getCId());
-			if (id_num == candidateSingle.getCId()) { // Will amend QAnswer objects with attributes.
-				candidateSingle.getCId();
-				candidateSingle.getQId();
-				candidateSingle.getAnswer();
-				
-				candidateSingleArrList.add(candidateSingle); // Will add the QAnswer object to the ArrayList
+			System.out.println("Candidate Profile: " + "CID: " + profile.getId() + " - Fname: " + profile.getFName());
 
-//				<<< Debugging Messages >>>
-//				System.out.println("CID: " + candidateSingle.getCId() + " - QID: " + 
-//				candidateSingle.getQId() + " - A: " + candidateSingle.getAnswer());
-			}
-		}
-		return candidateSingleArrList;
+		return profile;	
 	}
-	
-//	Functions as a "filter". The SET will collect unique candidate IDs from the stacked ArrayList.
-//	You can use this method to check which CIDs were taken out from the DB.
-//	By using an iterator with this function you can use it for candidate data extraction.
-	public Set<Integer> listAvlblCandidates(ArrayList<QAnswer> selectionList, ArrayList<QAnswer> answerList)
-	{
-		Set<Integer> avlblCID = new HashSet<Integer>(); // Will be used as a "filer" to create a set of unique CIDs from repetative data set.
-		ListIterator<QAnswer> candidateIterator = answerList.listIterator();
-		while(candidateIterator.hasNext())
-		{
-			QAnswer candidate = candidateIterator.next();			
-			avlblCID.add(candidate.getCId()); // Will add the occuring CIDs to a set (no duplicates allowed!)	
-		}
-		System.out.println("Available CIDs: " + avlblCID);
-		return avlblCID;
-	}
-*/	
 }
