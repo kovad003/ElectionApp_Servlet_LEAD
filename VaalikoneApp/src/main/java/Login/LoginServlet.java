b5ba417a9f3fee51956b01f1b06bc14e043be33a
ekaikas <78084737+ekaikas@users.noreply.github.com>
2021-04-11 12:50:48 +0300

    Welcome message

--- VaalikoneApp/src/main/java/Login/LoginServlet.java
+++ VaalikoneApp/src/main/java/Login/LoginServlet.java
@@ -2,6 +2,7 @@ package Login;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -9,71 +10,51 @@ import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import data.Candidate;
-
+import javax.servlet.http.HttpSession;
 
 /**
  * Servlet implementation class LoginServlet
  */
 @WebServlet("/LoginServlet")
 public class LoginServlet extends HttpServlet {
-	
 	private static final long serialVersionUID = 1L;
-	
-	private final String userID = "admin";
-	private final String password = "admin";
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public LoginServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
 
-	
-	protected void doPost(HttpServletRequest request,
-			HttpServletResponse response) throws ServletException, IOException {
-		
-		/*
-		 * TODO:
-		 * -> setup user/pass in db
-		 * -> boolean authenticate = false;
-		 * -> String user/pwd = read in those 2 fields
-		 * 
-		 * -> String sql = "SELECT * FROM electionmachine.candidates WHERE username=" + userID + " AND password=" + password + ";";
-		 * 
-		 * -> if((sql==null) || (sql.equals("") || sql.length()==0) {
-		 * authenticate = false;
-		 * }
-		 * else {
-		 * }...cookie cont from here
-		 * 
-		 * instead of else-> else if (sql.length()==1)
-		 * 
-		 * other way if that doesn't work:
-		 * leave if statement as is and just check that user and pwd in sql.
-		 */
-		
-		//set this to sql data
-		String user = request.getParameter("user");
-		String pwd = request.getParameter("pwd");
 
-		Candidate orban = new Candidate();
-		orban.setId(69);
-		orban.setUSERNAME(user);
-		orban.setPASSWORD(pwd);
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html");  
+		PrintWriter out = response.getWriter(); 
 		
-		System.out.println(orban.getId() + orban.getUSERNAME() + orban.getPASSWORD());
+		// get request parameters for name and pass
+		String n=request.getParameter("username");  
+		String p=request.getParameter("userpass"); 
 		
-		if(userID.equals(user) && password.equals(pwd)) {
-			Cookie loginCookie = new Cookie("user", orban.getUSERNAME());
-			Cookie phallos = new Cookie("id", String.valueOf(orban.getId()));
-			response.addCookie(loginCookie);
-			/*
-			 * if login ok -> 
-			 */
-			response.sendRedirect("_TEST_LOGIN_SUCCESS.jsp");
-		}else{
-			/*
-			 * if login not ok ->
-			 */
-			response.sendRedirect("loginPage.jsp");
-		}
+		
+		HttpSession session = request.getSession(false); //Creating a session
+		if(session!=null)
+			request.getSession(true).setAttribute("name", n);//setting session attribute
+		if(LoginDatabaseConnection.validate(n, p)){  
+			RequestDispatcher rd=request.getRequestDispatcher("index.jsp");  
+			rd.forward(request,response);  
 
-	}
-		
+		}  
+		else{  
+			out.print("<p style=\"color:red\">Sorry email or password error</p>");  
+			RequestDispatcher rd=request.getRequestDispatcher("loginPage.jsp");  
+			rd.include(request,response);  
+		}  
+
+		out.close();  
 	}
 
+}
